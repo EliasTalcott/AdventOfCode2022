49c4a89fc93d673c2fd7197909c134af053ac1bd
EliasTalcott <eli.talcott@gmail.com>
2023-09-09 18:13:10 -0400

    Update day 1 solution

--- 2022/day01/day01.py
+++ 2022/day01/day01.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python3
 """
 https://adventofcode.com/2022/day/1
 """
 
+import os
+
+
+with open(os.path.join(os.path.dirname(__file__), "calories.txt"), encoding="utf-8") as file:
+    elf_calories = [sum(map(int, elf.split("\n"))) for elf in file.read().split("\n\n")]
+
 # Part 1
-with open("calories.txt", encoding="utf-8") as file:
-    elves = [elf.split("\n") for elf in file.read().split("\n\n")]
-summed_elves = [sum([int(calories) for calories in elf]) for elf in elves]
-print(f"Elf with most calories has {max(summed_elves)}")
+most_calories = max(elf_calories)
+print(f"Elf with most calories has {most_calories}")
 
 # Part 2
-sorted_elves = sorted(summed_elves, reverse=True)
-print(f"Total for top 3 elves is: {sum(sorted_elves[:3])}")
+top_3_total = sum(sorted(elf_calories, reverse=True)[:3])
+print(f"Total for top 3 elves is: {top_3_total}")
